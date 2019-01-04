0be8c6bf3fbff55fb84e6639734254e9bbc12d79
giangAPears <43342277+giangAPears@users.noreply.github.com>
2019-01-04 17:32:59 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,27 @@
+//Spell #1
+
+$("#apple").click(function(){
+ $("#apple").fadeOut();
+});
+
+$(".ra").click(function(){
+ $("#apple").fadeIn();
+});
+
+
+//Spell #2
+
+$("#banana").click(function(){
+ $("#banana").slideUp();
+});
+
+
+
+//Spell #3
+
+$(".button").click(function(){
+ $("#watermelon").toggle();
+});
+
+
+
